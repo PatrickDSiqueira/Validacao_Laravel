6966ba72be3d6f49074acce851d53dbe164a5c93
Patrick Siqueira <psique.estuda@gmail.com>
2023-02-15 16:16:10 -0300

    Customização de mensagens - campo a campo

--- resources/views/novocliente.blade.php
+++ resources/views/novocliente.blade.php
@@ -24,36 +24,58 @@
                         @csrf
                         <div class="form-group">
                             <label for="nome">Nome do Cliente</label>
-                            <input type="text" name="nome" id="nome" class="form-control" placeholder="Nome do Cliente">
+                            <input type="text" name="nome" id="nome"
+                                   class="form-control {{$errors->has('nome')?"is-invalid":''}}"
+                                   placeholder="Nome do Cliente">
+                            @if($errors -> has('nome'))
+                                <div class="invalid-feedback is-invalid">
+                                    {{$errors->first('nome')}}
+                                </div>
+                            @endif
                         </div>
                         <div class="form-group">
                             <label for="idade">Idade do Cliente</label>
-                            <input type="number" name="idade" id="idade" class="form-control"
+                            <input type="number" name="idade" id="idade" class="form-control {{$errors->has('idade')?"is-invalid":''}}"
                                    placeholder="Idade do Cliente">
+                            @if($errors -> has('idade'))
+                                <div class="invalid-feedback is-invalid">
+                                    {{$errors->first('nome')}}
+                                </div>
+                            @endif
                         </div>
                         <div class="form-group">
                             <label for="endereco">Endereço do Cliente</label>
-                            <input type="text" class="form-control" name="endereco" id="endereco"
+                            <input type="text" class="form-control {{$errors->has('endereco')?"is-invalid":''}}" name="endereco" id="endereco"
                                    placeholder="Endereço do Cliente">
+                            @if($errors -> has('endereco'))
+                                <div class="invalid-feedback is-invalid">
+                                    {{$errors->first('nome')}}
+                                </div>
+                            @endif
                         </div>
                         <div class="form-group">
                             <label for="email">Email do Cliente</label>
-                            <input type="text" name="email" id="email" class="form-control"
+                            <input type="text" name="email" id="email" class="form-control {{$errors->has('email')?"is-invalid":''}}"
                                    placeholder="E-mail do Cliente">
+                            @if($errors -> has('email'))
+                                <div class="invalid-feedback is-invalid">
+                                    {{$errors->first('nome')}}
+                                </div>
+                            @endif
                         </div>
                         <button type="submit" class="btn btn-primary btn-sm">Salvar</button>
                         <a type="cancel" class="btn btn-danger btn-sm" href="/">Cancelar</a>
                     </form>
                 </div>
-                @if($errors->any())
+{{--                @if($errors->any())--}}
 
-                    <div class="div card-footer">
-                        @foreach($errors -> all() as $e)
-                            <div class="alert alert-danger" role="alert">{{$e}}</div>
-                        @endforeach
+{{--                    <div class="div card-footer">--}}
+{{--                        @foreach($errors -> all() as $e)--}}
+{{--                            <div class="alert alert-danger" role="alert">{{$e}}</div>--}}
+{{--                        @endforeach--}}
 
-                    </div>
-                @endif
+{{--                    </div>--}}
+{{--                @endif--}}
             </div>
 
         </div>
