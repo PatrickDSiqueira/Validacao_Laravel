093f3264deff30f0c325dc83c70fcf17e2bd0da0
Patrick Siqueira <psique.estuda@gmail.com>
2023-02-15 11:48:30 -0300

    Preparação do ambiente

---
+++ resources/views/Clientes.blade.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+    <title>Cadastro de Clientes</title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <meta name="csrf-token" content="{{csrf_token()}}">
+</head>
+<body>
+<script src="{{asset('js/app.css')}}" type="text/javascript"></script>
+</body>
+</html>
